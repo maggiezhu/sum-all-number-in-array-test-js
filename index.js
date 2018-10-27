a6136c3fa6409d9f3604447fb4f24341497264a4
maggiezhu <tong.zhu@mail.utoronto.ca>
2018-10-27 04:48:14 +0000

    recursion for nested array sum

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  for(let item of array){
+    console.log(item);
+    //base case
+    if(Array.isArray(item)){
+      sum+=sumItems(item);
+    }else{
+      sum+=item;
+    }
+  }
+  return sum;
 }
 
 module.exports = sumItems;
